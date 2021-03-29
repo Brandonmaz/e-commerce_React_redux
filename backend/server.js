894e1946b81b94af59a35d7700a791117dafec15
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-03-29 16:08:30 -0700

    setting up cart and cart screen page

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ import Data from "./Data.js";
 
 const app = express();
 
+app.get("/api/products/:id", (req, res) => {
+  const product = Data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product not Found" });
+  }
+});
 app.get("/api/products", (req, res) => {
   res.send(Data.products);
 });
