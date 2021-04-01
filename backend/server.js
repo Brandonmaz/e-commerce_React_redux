c8681210809aaca28ef63889193db01dc3371aa4
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-01 14:15:51 -0700

    set up signin and token backend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from "express";
 import mongoose from "mongoose";
-// import Data from "./Data.js";
+import dotenv from "dotenv";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
-
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 mongoose.connect(
   process.env.MONGODB_URL || "mongodb://localhost/e-commerce_react_redux",
   {
@@ -13,18 +15,6 @@ mongoose.connect(
     useCreateIndex: true,
   }
 );
-
-// app.get("/api/products/:id", (req, res) => {
-//   const product = Data.products.find((x) => x._id === req.params.id);
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ message: "Product not Found" });
-//   }
-// });
-// app.get("/api/products", (req, res) => {
-//   res.send(Data.products);
-// });
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.get("/", (req, res) => {
