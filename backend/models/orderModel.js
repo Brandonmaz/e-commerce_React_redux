b43237500d6f116c46e257713a20a840a033a02c
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-06 12:34:41 -0700

    updated paypal to work, orderModel, orderRouter to update pay

--- backend/models/orderModel.js
+++ backend/models/orderModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-
 const orderSchema = new mongoose.Schema(
   {
     orderItems: [
@@ -23,12 +22,18 @@ const orderSchema = new mongoose.Schema(
       country: { type: String, required: true },
     },
     paymentMethod: { type: String, required: true },
-    itemsPrice: { type: String, required: true },
-    shippingPrice: { type: String, required: true },
-    taxPrice: { type: String, required: true },
-    totalPrice: { type: String, required: true },
+    paymentResult: {
+      id: String,
+      status: String,
+      update_time: String,
+      email_address: String,
+    },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    isPaid: { type: Boolean, default: false },
+    isPaid: { type: Boolean, defalult: false },
     paidAt: { type: Date },
     isDelivered: { type: Boolean, default: false },
     deliveredAt: { type: Date },
