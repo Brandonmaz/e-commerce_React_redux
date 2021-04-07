f3cea52e5017a04f384496595c753cf398a782f1
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-07 15:15:29 -0700

    cretaed route for create product, applied it to create new product

--- backend/routers/productRouter.js
+++ backend/routers/productRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import Data from "../Data.js";
 import expressAsyncHandler from "express-async-handler";
 import Product from "../models/productModel.js";
+import { isAdmin, isAuth } from "../utils.js";
 
 const productRouter = express.Router();
 
@@ -44,6 +45,26 @@ productRouter.get(
     } catch (error) {
       res.send("Yeah! you have an error");
     }
+  })
+);
+productRouter.post(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const product = new Product({
+      name: "sample name" + Date.now(),
+      image: "/images/p1.jpeg",
+      price: 0,
+      category: "sample category",
+      brand: "sample brand",
+      countInStock: 0,
+      rating: 0,
+      numReviews: 0,
+      description: "sample description",
+    });
+    const createdProduct = await product.save();
+    res.send({ message: "Product Created", product: createdProduct });
   })
 );
 export default productRouter;
--- backend/utils.js
+++ backend/utils.js
@@ -36,7 +36,7 @@ export const isAuth = (req, res, next) => {
   }
 };
 export const isAdmin = (req, res, next) => {
-  if (req.user && req.userisAdmin) {
+  if (req.user && req.user.isAdmin) {
     next();
   } else {
     res.status(401).send({ message: "Invalid Admin Token" });
