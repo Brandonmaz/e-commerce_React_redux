ce842cbb50155c7fc7a9cd5a8f0f25e87c10bad8
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-07 13:41:20 -0700

    added admin routes

--- backend/utils.js
+++ backend/utils.js
@@ -35,3 +35,10 @@ export const isAuth = (req, res, next) => {
     res.status(401).send({ message: "No Token" });
   }
 };
+export const isAdmin = (req, res, next) => {
+  if (req.user && req.userisAdmin) {
+    next();
+  } else {
+    res.status(401).send({ message: "Invalid Admin Token" });
+  }
+};
