5c0c4adb0517df20daabe5c8d105ff90b3c5a2c1
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-09 15:01:00 -0700

    fixing heroku backend

--- backend/server.js
+++ backend/server.js
@@ -25,19 +25,22 @@ app.use("/api/orders", orderRouter);
 app.get("/api/config/paypal", (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID || "sb");
 });
+// app.get("/api/test", (req, res) => {
+// 	res.send(Data.users);
+// });
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/frontend/build/index.html"))
+  res.sendFile(path.join(__dirname, "frontend/build/index.html"))
 );
 // app.get("/", (req, res) => {
-//   res.send("Server is ready");
+// 	res.send("Server is ready");
 // });
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server at http://localhost:${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
 });
