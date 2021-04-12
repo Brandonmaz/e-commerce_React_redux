d7d9a576be2856db490b20381d56cb69f35e62b9
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-12 11:16:49 -0700

    created userList for admin, delete and edit buttons. Working on is Seller

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -3,7 +3,7 @@ import expressAsyncHandler from "express-async-handler";
 import bcrypt from "bcryptjs";
 import Data from "../Data.js";
 import User from "../models/userModel.js";
-import { generateToken, isAuth } from "../utils.js";
+import { generateToken, isAuth, isAdmin } from "../utils.js";
 const userRouter = express.Router();
 userRouter.get(
   "/seed",
@@ -86,6 +86,15 @@ userRouter.put(
         token: generateToken(updatedUser),
       });
     }
+  })
+);
+userRouter.get(
+  "/",
+  isAuth,
+  isAdmin,
+  expressAsyncHandler(async (req, res) => {
+    const users = await User.find({});
+    res.send(users);
   })
 );
 export default userRouter;
