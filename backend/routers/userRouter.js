245ec3e50b26b7b766d8949569d8986668c5e278
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-04-27 10:07:13 -0700

    made some changes to be able to push to heroku

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -14,26 +14,26 @@ userRouter.get(
     res.send(topSellers);
   })
 );
-userRouter.get(
-  "/seed",
-  expressAsyncHandler(async (req, res) => {
-    try {
-      const createdUsers = await User.insertMany(Data.users);
-      res.send({ createdUsers });
-    } catch (error) {
-      res.send("you have an error");
-      console.log(error);
-    }
-  })
-);
 // userRouter.get(
 //   "/seed",
 //   expressAsyncHandler(async (req, res) => {
-//     await User.remove({});
-//     const createdUsers = await User.insertMany(Data.users);
-//     res.send({ createdUsers });
+//     try {
+//       const createdUsers = await User.insertMany(Data.users);
+//       res.send({ createdUsers });
+//     } catch (error) {
+//       res.send("you have an error");
+//       console.log(error);
+//     }
 //   })
 // );
+userRouter.get(
+  "/seed",
+  expressAsyncHandler(async (req, res) => {
+    await User.remove({});
+    const createdUsers = await User.insertMany(Data.users);
+    res.send({ createdUsers });
+  })
+);
 userRouter.post(
   "/signin",
   expressAsyncHandler(async (req, res) => {
