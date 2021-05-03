b2a57cc535d3abf61c3eb4a16191c825641b54ee
Brandon Mazikowski <brandon.mazikowski@gmail.com>
2021-05-03 14:32:40 -0700

    adding AWS S3 for image uploading

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,6 +1,9 @@
 import multer from "multer";
-import express from "express";
+import express, { Router } from "express";
+import multerS3 from "multer-S3";
+import aws from "aws-sdk";
 import { isAuth } from "../utils.js";
+import config from "../config.js";
 
 const uploadRouter = express.Router();
 
@@ -19,5 +22,23 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 uploadRouter.post("/", isAuth, upload.single("image"), (req, res) => {
   res.send(`/${req.file.path}`);
+});
+aws.config.update({
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+});
+const s3 = new aws.S3();
+const storageS3 = multerS3({
+  s3,
+  bucket: "storeforyou-bucket",
+  acl: "public-read",
+  contentType: multerS3.AUTO_CONTENT_TYPE,
+  key(req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const uploadS3 = multer({ storage: storageS3 });
+uploadRouter.post("/s3", uploadS3.single("image"), (req, res) => {
+  res.send(req.file.location);
 });
 export default uploadRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,35 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import config from "./config.js";
 import path from "path";
 import productRouter from "./routers/productRouter.js";
 import userRouter from "./routers/userRouter.js";
 import orderRouter from "./routers/orderRouter.js";
 import uploadRouter from "./routers/uploadRouter.js";
-dotenv.config();
+
+const paypal = config.PAYPAL_CLIENT_ID;
+const googleApi = config.GOOGLE_API_KEY;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://localhost/e-commerce_react_redux",
-  {
+const mongodbUrl = config.MONGODB_URL;
+mongoose
+  .connect(mongodbUrl, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
-  }
-);
+  })
+  .catch((error) => console.log(error.reason));
 app.use("/api/uploads", uploadRouter);
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/orders", orderRouter);
 app.get("/api/config/paypal", (req, res) => {
-  res.send(process.env.PAYPAL_CLIENT_ID || "sb");
+  res.send(paypal);
 });
 app.get("/api/config/google", (req, res) => {
-  res.send(process.env.GOOGLE_API_KEY || "");
+  res.send(googleApi);
 });
 // app.get("/api/test", (req, res) => {
 // 	res.send(Data.users);
@@ -43,7 +46,7 @@ app.get("*", (req, res) =>
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
-const port = process.env.PORT || 5000;
+const port = config.PORT;
 app.listen(port, () => {
   console.log(`Serve at http://localhost:${port}`);
 });
